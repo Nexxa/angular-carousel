3325938f9e54bc0bd3c562769a3c31bd34fb1cd3
StefanoMagrassi <stefano.magrassi@gmail.com>
2015-08-31 11:38:50 +0200

    hotfix | bad fix for this issue https://github.com/revolunet/angular-carousel/issues/361

--- src/directives/rn-carousel.js
+++ src/directives/rn-carousel.js
@@ -374,7 +374,13 @@
 
                         if (iAttributes.rnCarouselControls!==undefined) {
                             // dont use a directive for this
-                            var canloop = ((isRepeatBased ? scope[repeatCollection.replace('::', '')].length : currentSlides.length) > 1) ? angular.isDefined(tAttributes['rnCarouselControlsAllowLoop']) : false;
+                            
+                            // @StefanoMagrassi 2015-08-31
+                            // Temporary bad fix to avoid error with controllerAs
+                            // No time for nice code - it just works...
+                            //var canloop = ((isRepeatBased ? scope[repeatCollection.replace('::', '')].length : currentSlides.length) > 1) ? angular.isDefined(tAttributes['rnCarouselControlsAllowLoop']) : false;
+                            var canloop = angular.isDefined(tAttributes['rnCarouselControlsAllowLoop']);
+                            
                             var nextSlideIndexCompareValue = isRepeatBased ? repeatCollection.replace('::', '') + '.length - 1' : currentSlides.length - 1;
                             var tpl = '<div class="rn-carousel-controls">\n' +
                                 '  <span class="rn-carousel-control rn-carousel-control-prev" ng-click="prevSlide()" ng-if="carouselIndex > 0 || ' + canloop + '"></span>\n' +
